b4002a782a3321cdabb7eb63aed42e57f3681256
harshjadhav28082008-dot <harshjadhav28082008@gmail.com>
2026-06-16 08:19:33 +0000

    create file and update file

---
+++ main.py
@@ -0,0 +1,60 @@
+#str
+x = "Hello World"
+print(type(x))
+print(x)	
+#int
+x = 20		
+print(type(x))
+print(x)
+#float	
+x = 20.5
+print(type(x))
+print(x)
+#complex
+x = 1j	
+print(type(x))
+print(x)
+#list
+x = ["apple", "banana", "cherry"]
+print(type(x))
+print(x)	
+#tuple
+x = ("apple", "banana", "cherry")
+print(type(x))
+print(x)	
+#range
+x = range(6)
+print(type(x))
+print(x)	
+#dict
+x = {"name" : "John", "age" : 36}
+print(type(x))
+print(x)	
+#set
+x = {"apple", "banana", "cherry"}
+print(type(x))
+print(x)	
+#frozenset
+x = frozenset({"apple", "banana", "cherry"})
+print(type(x))
+print(x)	
+#bool
+x = True	
+print(type(x))
+print(x)
+#bytes
+x = b"Hello"
+print(type(x))
+print(x)	
+#bytearray
+x = bytearray(5)
+print(type(x))
+print(x)	
+#memoryview
+x = memoryview(bytes(5))
+print(type(x))
+print(x)	
+#NoneType
+x = None	
+print(type(x))
+print(x)
