509f9208bb7aeaf6db5328fd5abc27462ca8bbd3
harshjadhav28082008-dot <harshjadhav28082008@gmail.com>
2026-06-16 13:05:40 +0000

    create file and update file

---
+++ task1.py
@@ -0,0 +1,22 @@
+thislist = ["apple", "banana", "kiwi", "melon", "mango"]
+print(thislist)
+
+
+thislist = ["apple", "banana", "cherry"]
+thislist[1] = "blackcurrant"
+print(thislist)
+
+
+thislist = ["apple", "banana", "kiwi", "melon", "mango"]
+thislist.append("orange")
+print(thislist)
+
+
+thislist = ["apple", "banana", "cherry", "kiwi", "melon", "mango"]
+del thislist[1]
+print(thislist)
+
+
+thislist = ["apple", "banana", "cherry", "kiwi", "melon", "mango"]
+del thislist
+print(thislist)
