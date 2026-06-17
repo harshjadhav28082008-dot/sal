6c2d0c6c25f1c6e79071bdc3c49965b79deca117
harshjadhav28082008-dot <harshjadhav28082008@gmail.com>
2026-06-17 07:34:16 +0000

    create file and update file

---
+++ list task1.py
@@ -0,0 +1,5 @@
+thislist = ["apple", "banana", "cherry", "kiwi", "melon", "mango"]
+thislist[1] = "blackcurrant"
+del thislist[1]
+print(thislist)
+del thislist
---
+++ tuple task1.py
@@ -0,0 +1,13 @@
+thistuple = ("apple", "banana", "cherry")
+y=list(thistuple)
+y.append("orange")
+thistuple= tuple(y)
+
+
+y = list(thistuple)
+y.remove("apple")
+thistuple = tuple(y)
+
+
+del thistuple
+print(thistuple)
