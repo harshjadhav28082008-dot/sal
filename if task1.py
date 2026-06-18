8352d366b2bab2456ee5124d2c1378d7b673f830
harshjadhav28082008-dot <harshjadhav28082008@gmail.com>
2026-06-18 08:07:09 +0000

    create file and update file

---
+++ if task1.py
@@ -0,0 +1,42 @@
+# a = 500
+# b = 25
+# if b > a:
+#   print("b is greater than a")
+# elif a == b:
+#   print("a and b are equal")
+# else:
+#   print("a is greater than b")
+
+
+# score = 50
+# attendance = 50
+# submitted = False
+
+# if score >= 40:
+#   if attendance >= 90:
+#     if submitted:
+#       print("Pass with good standing")
+#     else:
+#       print("Pass but missing assignment")
+#   else:
+#     print("Pass but low attendance")
+# else:
+#   print("Fail")
+
+
+fruit = 7
+match fruit:
+  case 1:
+    print("banana")
+  case 2:
+    print("apple")
+  case 3:
+    print("stawberry")
+  case 4:
+    print("blueberry")
+  case 5:
+    print("orange")
+  case 6:
+    print("watermelon")
+  case 7:
+    print("mango")
