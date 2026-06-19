440857d2f62ca8b79ea399d9cb9814f5826406dc
harshjadhav28082008-dot <harshjadhav28082008@gmail.com>
2026-06-19 07:33:32 +0000

    create file and update file

---
+++ for loop.py
@@ -0,0 +1,20 @@
+# for i in range(1, 11):
+#     print(i)
+
+
+# rows = 5
+
+# for i in range(1, rows + 1):
+#     print("*" * i)
+
+
+# rows = 5
+
+# for i in range(rows, 0, -1):
+#     print("*" * i)
+
+
+rows = int(input("Enter the number of rows: "))
+
+for i in range(1, rows + 1):
+    print("*" * i)
---
+++ while loop.py
@@ -0,0 +1,28 @@
+# i = 1
+
+# while i <= 10:
+#     print(i)
+#     i += 1
+
+
+# rows = 5
+# i = 1
+
+# while i <= rows:
+#     print("*" * i)
+#     i += 1
+
+
+# rows = 5
+# i = rows
+
+# while i >= 1:
+#     print("*" * i)
+#     i -= 1
+
+
+rows = int(input("Enter the number of rows: "))
+i = 1
+while i <= rows:
+    print("*" * i)
+    i += 1
