9ddeb80a9a5cc12d3c7002f5c9b0b3f72251edb8
harshjadhav28082008-dot <harshjadhav28082008@gmail.com>
2026-06-23 07:33:10 +0000

    create file and update file

---
+++ lambda.py
@@ -0,0 +1,30 @@
+# x = lambda a, b, c : a + b + c
+# print(x(5, 6, 2))
+
+
+# def myfunc(n):
+#   return lambda a : a * n
+
+# mydoubler = myfunc(2)
+
+# print(mydoubler(8))
+
+
+# def myfunc(n):
+#   return lambda a : a * n
+
+# mydoubler = myfunc(2)
+# mytripler = myfunc(3)
+
+# print(mydoubler(11))
+# print(mytripler(11))
+
+
+# numbers = [1, 2, 3, 4, 5]
+# doubled = list(map(lambda x: x * 2, numbers))
+# print(doubled)
+
+
+# numbers = [1, 2, 3, 4, 5, 6, 7, 8]
+# odd_numbers = list(filter(lambda x: x % 2 != 0, numbers))
+# print(odd_numbers)
